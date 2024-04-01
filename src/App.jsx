2a0185a97de06cfbd5fc7f69a5a2ae11437a1fb2
Saakshi Kobarne <saakshik15@gmail.com>
2024-04-01 10:39:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ButtonGradient from './assets/svg/ButtonGradient'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Benefits from './components/Benefits'
+import Collaboration from './components/Collaboration'
+
+function App() {
+  return (
+    <>
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+        <Header/>
+        <Hero/>
+        <Benefits/>
+        <Collaboration/>
+      </div>
+      <ButtonGradient/>
+    </>
+  )
+}
+
+export default App
